a084057937cd321bc16cf67b6beda0f0059039a5
Sunny <133204687+Sunny2567@users.noreply.github.com>
2023-08-01 12:09:29 +0000

    Solve the problem of garbled Chinese names

--- backend/utils/files/multer.js
+++ backend/utils/files/multer.js
@@ -1,14 +1,14 @@
 function setupMulter() {
   const multer = require("multer");
 
-  // Handle File uploads for auto-uploading.
+    // Handle File uploads for auto-uploading.
   const storage = multer.diskStorage({
     destination: function (_, _, cb) {
       const path = require("path");
       const uploadOutput = path.resolve(
         __dirname,
-        `../../../document-processor/hotdir`
-      );
+         `../../../document-processor/hotdir`
+         );
       cb(null, uploadOutput);
     },
     filename: function (_, file, cb) {
@@ -17,6 +17,11 @@ function setupMulter() {
   });
   const upload = multer({
     storage,
+    fileFilter: function (req, file, callback) {
+      // Solve the problem of garbled Chinese names
+      file.originalname = Buffer.from(file.originalname, "latin1").toString("utf8");
+      callback(null, true);
+    },
   });
   return { handleUploads: upload };
 }
