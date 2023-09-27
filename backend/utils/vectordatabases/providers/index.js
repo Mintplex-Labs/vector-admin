bb522d7b330aa3696e77067f4a5be3a99fdb7abd
timothycarambat <rambat1010@gmail.com>
2023-09-26 18:48:48 -0700

    import at runtime vectorDB providers

--- backend/utils/vectordatabases/providers/index.js
+++ backend/utils/vectordatabases/providers/index.js
@@ -1,29 +1,28 @@
-const {
-  OrganizationConnection,
-} = require("../../../models/organizationConnection");
-const { Chroma } = require("./chroma");
-const { Pinecone } = require("./pinecone");
-const { QDrant } = require("./qdrant");
-const { Weaviate } = require("./weaviate");
-
 function selectConnector(organizationConnector) {
+  const {
+    OrganizationConnection,
+  } = require("../../../models/organizationConnection");
   const { type } = organizationConnector;
 
   if (!OrganizationConnection.supportedConnectors.includes(type))
     throw new Error("Unsupported connector for vector database.");
   if (organizationConnector.type === "chroma") {
+    const { Chroma } = require("./chroma");
     return new Chroma(organizationConnector);
   }
 
   if (organizationConnector.type === "pinecone") {
+    const { Pinecone } = require("./pinecone");
     return new Pinecone(organizationConnector);
   }
 
   if (organizationConnector.type === "qdrant") {
+    const { QDrant } = require("./qdrant");
     return new QDrant(organizationConnector);
   }
 
   if (organizationConnector.type === "weaviate") {
+    const { Weaviate } = require("./weaviate");
     return new Weaviate(organizationConnector);
   }
 
