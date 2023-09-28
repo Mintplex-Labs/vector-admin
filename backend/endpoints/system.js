5bfe70238922b3ccd8874a3c18b6203a24ff3e7c
timothycarambat <rambat1010@gmail.com>
2023-09-28 15:41:26 -0700

    fix backend migrate command

--- backend/endpoints/system.js
+++ backend/endpoints/system.js
@@ -38,7 +38,7 @@ function systemEndpoints(app) {
 
   app.get("/migrate", async (_, response) => {
     const execSync = require("child_process").execSync;
-    execSync("npx prisma migrate dev --name init", {
+    execSync("npx prisma migrate deploy --schema=./prisma/schema.prisma", {
       stdio: "inherit",
     });
     response.sendStatus(200).end();
