0e0fda0956aea19a4fdad883d29f5c3c7b926d00
shatfield4 <seanhatfield5@gmail.com>
2023-12-27 16:17:22 -0800

    Create new organization modal complete

--- frontend/src/components/Sidebar/CreateOrganizationModal/index.tsx
+++ frontend/src/components/Sidebar/CreateOrganizationModal/index.tsx
@@ -18,10 +18,13 @@ export default function CreateOrganizationModal() {
   };
 
   return (
-    <dialog id="organization-creation-modal" className="w-1/3 rounded-lg">
-      <div className="w-full overflow-y-scroll rounded-sm bg-white p-[20px]">
+    <dialog
+      id="organization-creation-modal"
+      className="w-1/3 rounded-xl border-2 border-white/20 bg-main shadow"
+    >
+      <div className="w-full overflow-y-scroll rounded-sm p-[20px]">
         <div className="px-6.5 py-4">
-          <h3 className="font-medium text-black dark:text-white">
+          <h3 className="text-lg font-medium text-white">
             Create a New Organization
           </h3>
         </div>
@@ -35,7 +38,7 @@ export default function CreateOrganizationModal() {
           <form onSubmit={handleSubmit}>
             <div className="px-6.5">
               <div className="mb-4.5">
-                <label className="mb-2.5 block text-black dark:text-white">
+                <label className="mb-2.5 block text-sm font-medium text-white">
                   Organization Name
                 </label>
                 <input
@@ -44,15 +47,15 @@ export default function CreateOrganizationModal() {
                   name="name"
                   placeholder="My Organization"
                   autoComplete="off"
-                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                  className="placeholder-text-white/60 w-full rounded-lg border border-white/10 bg-main-2 px-2.5 py-2 text-sm text-white"
                 />
               </div>
               <div className="flex flex-col gap-y-2">
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded bg-blue-500 p-3 font-medium text-white"
+                  className="w-full rounded-lg bg-white p-2 font-medium text-main shadow-lg transition-all duration-300 hover:scale-105 hover:bg-opacity-90"
                 >
-                  Create Organization
+                  Create Organization &rarr;
                 </button>
                 <button
                   type="button"
@@ -61,12 +64,12 @@ export default function CreateOrganizationModal() {
                       .getElementById('organization-creation-modal')
                       ?.close();
                   }}
-                  className="flex w-full justify-center rounded bg-transparent p-3 font-medium text-slate-500 hover:bg-slate-200"
+                  className="w-full rounded-lg bg-transparent p-2 font-medium text-white transition-all duration-300 hover:bg-red-500/80 hover:bg-opacity-90 hover:text-white"
                 >
                   Cancel
                 </button>
               </div>
-              <p className="my-2 rounded-lg border border-orange-800 bg-orange-100 p-2 text-center text-sm text-orange-800">
+              <p className="my-2 rounded-lg border border-white/20 bg-main-2 p-2 text-center text-sm text-white">
                 Once your organization exists you can start workspaces and
                 documents.
               </p>
